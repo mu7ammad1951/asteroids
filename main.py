d60e6c6519d2d136ce55f671b97cf85f228733c6
AbdulKareem Mohammed <mabkareem@gmail.com>
2025-01-13 17:52:35 -0500

    Add circleshape class and player class

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     
+    player = Player(SCREEN_HEIGHT / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for _ in updatable:
+            _.update(dt)
+
         screen.fill("black")
+
+        for _ in drawable:
+            _.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+
+
 if __name__ == "__main__":
     main()
